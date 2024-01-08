cc84544f8cb8841fbec14d6a0347ac466d3f4275
IT-workers713 <rayehriad99@gmail.com>
2024-01-08 13:03:57 -1000

    experiementation utilisation methode title et capitolize algo qui affiche le mot de passe si correcte ou non algo qui affiche les nombres dans une liste execercice afficher le nom capitolize

--- expirement.py
+++ expirement.py
@@ -1,7 +1,9 @@
 
-while True:
-    text = "enter le todo a faire "
-    todo = input(text)
-    print(todo.title())
-
+#while True:
+   # text = "enter le todo a faire "
+   # todo = input(text)
+   # print(todo.title())
+prompt = "enter vos nom : \n"
+text = input(prompt)
+print(text.capitalize())
 
