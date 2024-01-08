ea8c2ec477c11aa76d6d8358a2b1bf3160d5808d
IT-workers713 <rayehriad99@gmail.com>
2024-01-08 13:48:44 -1000

    exo2

--- bonus3.py
+++ bonus3.py
@@ -1,20 +1,3 @@
-#repas =['pasta','pizza']
-
-#for meal in repas:
-   # print(meal.capitalize())
-
-#print("by")
-#excercice1:
-count = []
-country = input("entrer votre pays\n")
-match country:
-    case 'USA':
-        print("hello ")
-
-    case 'INDIA':
-        print("namaste")
-
-    case 'GERMANY':
-        print("germany")
-
-print("by")
+ingredients = ["john smith", "sen plakay", "dora ngacely"]
+for ing in ingredients:
+    print(ing)
