6749610a79a5561ce432fd48f44482873bce8df4
IT-workers713 <rayehriad99@gmail.com>
2024-01-08 11:30:17 -1000

    code 1 jour1

---
+++ bonus1.py
@@ -0,0 +1,3 @@
+input_nom = input("enter le text ")
+nbr = len(input_nom)
+print("le nombre des lettres dans le texte ecrits est :",nbr)
---
+++ todo.py
@@ -0,0 +1,10 @@
+user_prompt = "ENTREZ UN TODO :\n"
+todo1 = input(user_prompt)
+todo2 = input(user_prompt)
+todo3 = input(user_prompt)
+
+# multiple input functions
+# list pour afficher les todos
+todos = [todo1,todo2,todo3,"hello "]
+print(todos)
+
