c0e4c483c1b2e71c96cf4b889cf8e5a2874834ad
IT-workers713 <rayehriad99@gmail.com>
2024-01-08 12:15:35 -1000

    experiementation utilisation methode title et capitolize algo qui affiche le mot de passe si correcte ou non algo qui affiche les nombres dans une liste

---
+++ bonus2.py
@@ -0,0 +1,6 @@
+password = input(" enter mot de passe ")
+
+while password !="password":
+    password = input(" enter mot de passe ")
+
+print("password est correct")
---
+++ bonus22.py
@@ -0,0 +1,6 @@
+x = 1
+number=[x]
+while x<=6:
+    print(number)
+    x=x+1
+    number.append(x)
