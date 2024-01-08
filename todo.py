6534a65e772868ba8fb65fdbe3bc5d51784eee3a
IT-workers713 <rayehriad99@gmail.com>
2024-01-08 13:27:04 -1000

    experiementation utilisation methode title et capitolize algo qui affiche le mot de passe si correcte ou non algo qui affiche les nombres dans une liste execercice afficher le nom capitolize solution sans boucle solution deux avec boucle 3day add and show function

--- todo.py
+++ todo.py
@@ -1,10 +1,17 @@
-user_prompt = "ENTREZ UN TODO :\n"
-todo1 = input(user_prompt)
-todo2 = input(user_prompt)
-todo3 = input(user_prompt)
+todos = []
+while True :#loop (true is boolean)
+    user_action = input("type add or show\n")
+    match user_action:
+        case 'add':
+            todo = input("enter todo")
+            todos.append(todo)
+        case 'show':
+            print(todos)
 
-# multiple input functions
-# list pour afficher les todos
-todos = [todo1,todo2,todo3,"hello "]
-print(todos)
+
+
+
+
+
+
 
