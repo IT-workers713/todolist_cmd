1d5386f5e87db7ef26e44ebbe4dbedc5c2c09419
IT-workers713 <rayehriad99@gmail.com>
2024-01-08 13:46:15 -1000

    experiementation utilisation methode title et capitolize algo qui affiche le mot de passe si correcte ou non algo qui affiche les nombres dans une liste execercice afficher le nom capitolize solution sans boucle solution deux avec boucle 3day add and show function

---
+++ bonus3.py
@@ -0,0 +1,20 @@
+#repas =['pasta','pizza']
+
+#for meal in repas:
+   # print(meal.capitalize())
+
+#print("by")
+#excercice1:
+count = []
+country = input("entrer votre pays\n")
+match country:
+    case 'USA':
+        print("hello ")
+
+    case 'INDIA':
+        print("namaste")
+
+    case 'GERMANY':
+        print("germany")
+
+print("by")
