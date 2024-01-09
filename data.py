0bca17de319d6d61a67885ab6c3c795e7b4a6e83
IT-workers713 <rayehriad99@gmail.com>
2024-01-09 08:16:00 -1000

    exercice2

--- data.py
+++ data.py
@@ -1,3 +1,5 @@
-filenames = ['document', 'report', 'presentation']
-for index,filename in enumerate(filenames):
-    print(f"{index}-{filename.capitalize()}.txt")
+ips = ['100.122.133.105', '100.122.133.111']
+
+user_input = int(input("enter your ip address"))
+msg = f"your choice {ips[user_input]}"
+print(msg)
