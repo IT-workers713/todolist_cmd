b03a856d257d8c1fa866f4485a39280f2c275b88
IT-workers713 <rayehriad99@gmail.com>
2024-01-09 13:02:29 -1000

    ex3

--- data.py
+++ data.py
@@ -1,3 +1,11 @@
-file = open('essay.txt','r')
-td=file.read()
-print(len(td))
+member = input("Add a new member: ")
+
+file = open("members.txt", 'r')
+existing_members = file.readlines()
+file.close()
+
+existing_members.append(member + "\n")
+
+file = open("members.txt", 'w')
+existing_members = file.writelines(existing_members)
+file.close()
