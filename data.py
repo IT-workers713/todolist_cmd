d3b3fd8f81bb337a6cbfe5a5399b1e833a6472a0
IT-workers713 <rayehriad99@gmail.com>
2024-01-09 12:55:14 -1000

    ex2

--- data.py
+++ data.py
@@ -1,5 +1,3 @@
-ips = ['100.122.133.105', '100.122.133.111']
-
-user_input = int(input("enter your ip address"))
-msg = f"your choice {ips[user_input]}"
-print(msg)
+file = open('essay.txt','r')
+td=file.read()
+print(len(td))
