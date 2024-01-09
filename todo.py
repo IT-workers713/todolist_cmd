affbc728fdbfc5c72070cd66ed5f9fcc4a628d0f
IT-workers713 <rayehriad99@gmail.com>
2024-01-08 14:19:41 -1000

    fonction edit

--- todo.py
+++ todo.py
@@ -1,14 +1,23 @@
 todos = []
 while True :#loop (true is boolean)
-    user_action = input("type add or show or exit\n")
+    user_action = input("type add or show or edit or exit\n")
+    user_action = user_action.strip()
     match user_action:
         case 'add':
             todo = input("enter todo \n")
             todos.append(todo)
         case 'show':
-            print(todos)
+            for item in todos:
+                print(item)
+        case 'edit':
+            nbr = int(input("enter le nombre de todo pour le modifier"))
+            nbr = nbr-1
+            newtodo = input("enter un nv todo")
+            todos[nbr] = newtodo
+
         case 'exit':
             break
+print("Good Bye !! ")
 
 
 
