53ed12997e9d96305f26d9cfd4cfc395a0dcf07a
IT-workers713 <rayehriad99@gmail.com>
2024-01-10 13:20:54 -1000

    ex3

--- data.py
+++ data.py
@@ -1,6 +1,4 @@
-usernames = ["john 1990", "alberta1970", "magnola2000"]
+user_entries = ['10', '19.1', '20']
 
-user = [len(username) for username in usernames]
-print(user)
-
-
+liste=[float(item) for item in user_entries]
+print(liste)
