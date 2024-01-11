4aee740530b03e0f487c31e96328b6311d773011
IT-workers713 <rayehriad99@gmail.com>
2024-01-11 03:30:12 -1000

    part 2 ex2

--- data.py
+++ data.py
@@ -6,3 +6,5 @@ if len(password) > 7:
 #If the password has 7 or fewer characters, the program returns, "Your password is weak".
 elif len(password)<7:
     print("Your password is weak")
+elif len(password)==7:
+    print("password to weak you need to enter another password")
