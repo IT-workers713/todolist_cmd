cb9ad0ef23ecfa6020adc5a588606bfca2747080
IT-workers713 <rayehriad99@gmail.com>
2024-01-12 00:16:05 -1000

    ex1

--- data.py
+++ data.py
@@ -1,15 +1,3 @@
-password= input("enter new password \n")
-
-def best_password():
-    if len(password) < 8:
-        print("password weak")
-    elif len(password)==8:
-        print("password weak")
-    elif any(p.isupper() for p in password ) and len(password) > 8 and any(p.isdigit() for p in password):
-        print("password strong")
-    else:
-        print("password weak")
-
-best_password()
-
-
+def calculate(year,current=2024):
+    age = current-year
+    return age
