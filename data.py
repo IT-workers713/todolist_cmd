6af24067c7a01945788f9d07632f7adea95d304a
IT-workers713 <rayehriad99@gmail.com>
2024-01-11 23:52:34 -1000

    Ex02

--- data.py
+++ data.py
@@ -1,10 +1,15 @@
-def convert():
-    liters = int(input("enter leters"))
-    res = liters/1000
-    message = f"{liters} liters == {res} cubic meter"
-    return message
+password= input("enter new password \n")
 
+def best_password():
+    if len(password) < 8:
+        print("password weak")
+    elif len(password)==8:
+        print("password weak")
+    elif any(p.isupper() for p in password ) and len(password) > 8 and any(p.isdigit() for p in password):
+        print("password strong")
+    else:
+        print("password weak")
 
-print(convert())
+best_password()
 
 
