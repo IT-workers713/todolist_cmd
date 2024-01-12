9ed71f242ddd2b58c5f7fd6c5a24cfc6644b1a02
IT-workers713 <rayehriad99@gmail.com>
2024-01-12 00:21:25 -1000

    ex3

--- data.py
+++ data.py
@@ -2,4 +2,6 @@ def calculate(year,current=2024):
     age = current-year
     return age
 calcul = int(input("enter votre anné de naissance : "))
-print(calculate(calcul))
+print(calculate(calcul,2024))
+if calculate(calcul) > 120:
+    print("vous etes chickour")
