ba8a582fe004001c8365d48ee4e6c6d32a7459c5
IT-workers713 <rayehriad99@gmail.com>
2024-01-13 08:49:01 -1000

    final commit

--- todo.py
+++ todo.py
@@ -1,5 +1,8 @@
 #from functions import get_todos,write_todos
 import functions
+import time
+now = time.strftime("%b %d ,%Y %H:%M:%S")
+print("It is ", now)
 while True:
     user_action = input("Type add or show or edit or complete or exit\n")
     user_action = user_action.strip()
