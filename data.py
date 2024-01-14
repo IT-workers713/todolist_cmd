5ccf289f75693f8cd957722a12532046b281f182
IT-workers713 <rayehriad99@gmail.com>
2024-01-14 06:23:13 -1000

    day16 GUI INTERFACE excercices

--- data.py
+++ data.py
@@ -1,14 +1,11 @@
-def water_state(tempature):
-    x=0
-    y=100
-    if tempature <=x:
-        return "SOLID"
-    elif x<tempature<y:
-        return "liquid"
-    else:
-        return "gaz"
-temperatures = float(input("entrer temperature of water"))
+import PySimpleGUI as sg
 
-state = water_state(temperatures)
-print(state)
+label = sg.Text("enter feet")
+input_text1 = sg.InputText(tooltip="enter feet")
+label2 = sg.Text("enter inches")
+input_text2 = sg.InputText(tooltip="enter inches")
 
+window =sg.Window("convertor",layout=[[label,input_text1],[label2,input_text2]])
+
+window.read()
+window.close()
