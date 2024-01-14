6d02bd6aae6c42f60b83f9e6b4528f2072f1bcdd
IT-workers713 <rayehriad99@gmail.com>
2024-01-14 13:31:47 -1000

    day 17

--- gui.py
+++ gui.py
@@ -2,9 +2,29 @@ import functions
 
 import PySimpleGUI as sg
 label = sg.Text("TYPE IN A TODO ")
-input_box = sg.InputText(tooltip="ENTER A TODO ")
+input_box = sg.InputText(tooltip="ENTER A TODO ",key="todo")
 add_button = sg.Button("ADD")
-window=sg.Window("MY TODO APP",layout=[[label],[input_box,add_button]])
 
-window.read()
+window=sg.Window("MY TODO APP",
+                 layout=[[label],[input_box,add_button]],
+                 font=('Times New Roman',20))
+
+
+while True:
+    event,values=window.read()
+    print(event)
+    print(values)
+    match event:
+        case "ADD":
+            todos = functions.get_todos()
+            nv = values['todo'] + "\n"
+            todos.append(nv)
+            functions.write_todos(todos)
+        case sg.WINDOW_CLOSED:
+            break
+
+
+
+
+
 window.close()
